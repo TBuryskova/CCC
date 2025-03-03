add82913e28fb4b1d795985ca4accec9ef53cec3
TBuryskova <tburyskova@gmail.com>
2025-03-03 09:04:15 -0800

    reorganized regressions

--- regressions_case_cluster_chamber.R
+++ regressions_case_cluster_chamber.R
@@ -43,15 +43,15 @@ cited_per_year_C <- lm(FE_cpy~ n_applicants+
                          + average_yob+ var_yob +same_background+all_different_background+scholar+same_uni+gender,  data_cleanCE)
 
 
-clustered_se <- cluster.vcov(length_proceeding, cbind(data_cleanCE$judge1, data_cleanCE$judge2, data_cleanCE$judge3))
-clustered_se_C <- cluster.vcov(length_proceeding_C, cbind(data_cleanCE$judge1, data_cleanCE$judge2, data_cleanCE$judge3))
+clustered_se <- cluster.vcov(length_proceeding, data_cleanCE$chamber_id)
+clustered_se_C <- cluster.vcov(length_proceeding_C, data_cleanCE$chamber_id)
 
 # Repeat for the other models
-clustered_se_outcome <- cluster.vcov(outcome, cbind(data_cleanCE$judge1, data_cleanCE$judge2, data_cleanCE$judge3))
-clustered_se_outcome_C <- cluster.vcov(outcome_C, cbind(data_cleanCE$judge1, data_cleanCE$judge2, data_cleanCE$judge3))
+clustered_se_outcome <- cluster.vcov(outcome, data_cleanCE$chamber_id)
+clustered_se_outcome_C <- cluster.vcov(outcome_C, data_cleanCE$chamber_id)
 
-clustered_se_cpy <- cluster.vcov(cited_per_year, cbind(data_cleanCE$judge1, data_cleanCE$judge2, data_cleanCE$judge3))
-clustered_se_cpy_C <- cluster.vcov(cited_per_year_C, cbind(data_cleanCE$judge1, data_cleanCE$judge2, data_cleanCE$judge3))
+clustered_se_cpy <- cluster.vcov(cited_per_year, data_cleanCE$chamber_id)
+clustered_se_cpy_C <- cluster.vcov(cited_per_year_C, data_cleanCE$chamber_id)
 
 stargazer(length_proceeding, length_proceeding_C,outcome, outcome_C,cited_per_year, cited_per_year_C,
           se = list(sqrt(diag(clustered_se)), sqrt(diag(clustered_se_C)), sqrt(diag(clustered_se_outcome)), sqrt(diag(clustered_se_outcome_C)), sqrt(diag(clustered_se_cpy)), sqrt(diag(clustered_se_cpy_C))),
