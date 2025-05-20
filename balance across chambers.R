dcdd45da2ae8a9812a8f01a9a338575530d56a8b
TBuryskova <tburyskova@gmail.com>
2025-05-20 13:23:43 +0200

    balance plots

--- balance across chambers.R
+++ balance across chambers.R
@@ -15,7 +15,7 @@ data_clean <- read_rds("data_clean.rds")
 
 
 ########### chamber_id balance ########
-balance <- data_clean%>% group_by(chamber_id) %>% filter(n()>100) %>%
+balance <- data_clean%>% group_by(chamber_id) %>%
   ungroup() %>%
   rowwise() %>%
   mutate(
@@ -31,19 +31,58 @@ ungroup() %>%
   
 ggplot(balance, aes(x=chamber_id, y = mean_n)) +
   geom_bar(fill="skyblue", stat='summary', fun='mean') +
-labs(
-    y = "Number of Concerned Acts",
-    x = "Chamber"
-  ) + 
+ 
   geom_errorbar(aes(ymin = mean_n - se_n, ymax = mean_n + se_n), width = 0.2) +
   labs(
     y = "Number of Applicants",
     x = "Chamber"
   ) +
-  theme_minimal()
+  theme_minimal() + theme(axis.text.x =  element_blank())
+ggsave("balance_n_applicants.png")
+
+balance <- data_clean%>% group_by(chamber_id) %>%
+  ungroup() %>%
+  rowwise() %>%
+  group_by(chamber_id) %>%
+  summarize(
+    mean_n = mean(n_concerned_act, na.rm = TRUE),
+    se_n = sd(n_concerned_act, na.rm = TRUE) / sqrt(n())
+  )
+
+ggplot(balance, aes(x=chamber_id, y = mean_n)) +
+  geom_bar(fill="skyblue", stat='summary', fun='mean') +
+  
+  geom_errorbar(aes(ymin = mean_n - se_n, ymax = mean_n + se_n), width = 0.2) +
+  labs(
+    y = "Number of Concerned Acts",
+    x = "Chamber"
+  ) +
+  theme_minimal() + theme(axis.text.x =  element_blank())
+ggsave("balance_n_acts.png")
+
+balance <- data_clean%>% group_by(chamber_id) %>%
+  ungroup() %>%
+  rowwise() %>%
+  group_by(chamber_id) %>%
+  summarize(
+    mean_n = mean(controversial, na.rm = TRUE),
+    se_n = sd(controversial, na.rm = TRUE) / sqrt(n())
+  )
+
+ggplot(balance, aes(x=chamber_id, y = mean_n)) +
+  geom_bar(fill="skyblue", stat='summary', fun='mean') +
+  
+  geom_errorbar(aes(ymin = mean_n - se_n, ymax = mean_n + se_n), width = 0.2) +
+  labs(
+    y = "Proportion of controversial cases",
+    x = "Chamber"
+  ) +
+  theme_minimal() + theme(axis.text.x =  element_blank())
+ggsave("balance_controversial.png")
+
 
 dow <- data_clean  %>%
-  group_by(chamber_id) %>% filter(n()>100) %>%
+  group_by(chamber_id) %>% 
   mutate(dow=wday(ymd(date_submission))) %>% ungroup()
 
 ggplot(dow, aes(x = factor(dow), fill = chamber_id)) +
@@ -57,30 +96,20 @@ ggplot(dow, aes(x = factor(dow), fill = chamber_id)) +
     x = "Day of the Week"
   ) +
   facet_wrap(~ chamber_id, ncol = 4) +  # Force vertical layout
-  theme_minimal()
-
+  theme_minimal() + theme(legend.position = "none", strip.text = element_blank()) 
+ggsave("balance_day_of_week.png")
 
 balance <- data_clean
 
 
 aov(n_applicants ~ chamber_id, balance) %>% summary()
 aov(dow ~ chamber_id, dow) %>% summary()
+aov(controversial ~ chamber_id, balance) %>% summary()
 
 balance <- data_clean%>% group_by(chamber_id) %>%
   summarise(n=n())
 
 
-ggplot(balance, aes(n)) +
-  geom_histogram( binwidth=5, fill = "skyblue", color = "black") +
-  
-  labs(
-    x = "Number of Cases",
-    y = "Number of Chambers"
-  ) +
-  theme( text=element_text(size=20),
-         panel.grid.major = element_blank(),  
-         panel.grid.minor = element_blank(),
-         panel.background = element_blank())
 
 ggplot(balance, aes(chamber_id, n)) +
   geom_col( fill = "skyblue", color = "black") +
@@ -90,7 +119,8 @@ ggplot(balance, aes(chamber_id, n)) +
     y = "Number of Cases"
   ) +
   theme( text=element_text(size=20),
-         axis.text.x = element_blank(),
          panel.grid.major = element_blank(),  
          panel.grid.minor = element_blank(),
-         panel.background = element_blank())
+         panel.background = element_blank()) + 
+  theme(axis.text.x =  element_blank())
+ggsave("balance_n_cases.png")
